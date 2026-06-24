e934a9733b139b07b1dbffc32ed3e10e4c18c527
zlshames <zlshames@gmail.com>
2026-06-24 19:15:37 -0400

    fix: typing service startup

--- lib/helpers/backend/startup_tasks.dart
+++ lib/helpers/backend/startup_tasks.dart
@@ -121,9 +121,6 @@ class StartupTasks {
     await ChatsSvc.init(headless: headless);
     Logger.info("ChatsService ready");
 
-    Logger.info("Registering TypingIndicatorService...");
-    GetIt.I.registerSingleton<TypingIndicatorService>(TypingIndicatorService());
-    Logger.info("TypingIndicatorService ready");
   }
 
   static Future<void> _initHttpService() async {
@@ -236,6 +233,7 @@ class StartupTasks {
 
     Logger.info("Registering ChatsService, SocketService, and NotificationsService...");
     GetIt.I.registerSingleton<ChatsService>(ChatsService());
+    GetIt.I.registerSingleton<TypingIndicatorService>(TypingIndicatorService());
     GetIt.I.registerSingleton<SocketService>(SocketService());
     await _waitForInterop(notifications: true);
 
